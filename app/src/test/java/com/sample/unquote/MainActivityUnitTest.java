a219a44272b783c6dbe0abf1b80e880c2c355a07
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-10-31 06:58:54 +0100

    add roboelectric

--- app/src/test/java/com/sample/unquote/MainActivityUnitTest.java
+++ app/src/test/java/com/sample/unquote/MainActivityUnitTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityUnitTest {
 
     MainActivity mainActivity;
